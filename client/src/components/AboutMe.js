3158b0faeab313d706baee3b3ba249a3ac3a9ed2
lilanika <68299182+lilanika@users.noreply.github.com>
2021-03-07 11:06:30 +0100

    adding the edit page

--- client/src/components/AboutMe.js
+++ client/src/components/AboutMe.js
@@ -12,8 +12,11 @@ export default class AboutMe extends Component{
     return (
       <div>
         <Navbar/>
-        <li><Link to="/aboutme">About me</Link></li>
-        <li><Link to="/languages">Languages</Link></li>
+        <h2>Hello</h2>
+        <div>
+        <li><Link to="/aboutme"  className="btn-dark"> About me</Link></li>
+        <Link to="/languages" className="btn-dark">Languages</Link>
+        </div>
         <h1>About me</h1>
         <h2>{this.props.user.name}</h2>
         <p>{this.props.user.age} years odl</p>
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -9,11 +9,29 @@ export default class Profile extends Component{
   render() {
     return (
       <div>
-        <Navbar/>
-        <h1>You are on profil user</h1>
-        <li><Link to="/aboutme">About me</Link></li>
-        <li><Link to="/languages">Languages</Link></li>
+        <Navbar/>  
+        <div className="profile-grid my-1">
+        <div className="profile-exp bg-white p-2">
+        <h1>Edit your Profile </h1>
+        <li><Link to="/aboutme" className="btn-dark">About me</Link></li>
+        <li><Link to="/languages" className="btn-dark">Languages</Link></li>
+        </div>
+        <div className="profile-edu bg-white p-2">
+        <h1>About me</h1>
+        <h2>{this.props.user.name}</h2>
+        <p>{this.props.user.age} years odl</p>
+        <p> Gender: {this.props.user.gender}</p>
+        <p>Live in {this.props.user.location}</p>
+        <p>Description: {this.props.user.description}</p>
+        <p>Goal: {this.props.user.goal}</p>
+
+      
+        </div>
+        </div>
+
+
       </div>
+    
       
       
     )
