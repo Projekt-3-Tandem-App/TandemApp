b2d85ac9dde42fddfcc323cefedef043e12ed0be
lilanika <68299182+lilanika@users.noreply.github.com>
2021-03-11 14:50:06 +0100

    work on deployment

--- app.js
+++ app.js
@@ -144,4 +144,14 @@ app.use('/api/message', message);
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
+// for deployment
+/*
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
+*/ 
 module.exports = app;
