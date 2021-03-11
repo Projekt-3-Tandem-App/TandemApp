a305f9d47e3ed91f7b41ed7a98bb0cecda191cae
orianemgn <mgnoriane@gmail.com>
2021-03-12 00:42:34 +0100

    little modif

--- client/src/components/AboutPapagei.js
+++ client/src/components/AboutPapagei.js
@@ -17,23 +17,21 @@ export default class AboutPapagei extends Component {
           <div>
             <p>Since we believe that the best way to learn, 
               progress and master a language is to exchange, 
-              we want to help you find your perfect tandem partner on our Papagei 
+              we want to help you find your perfect Tandem partner on our Papagei 
               application </p>
           </div>
 
           <div>
-          <p>On our application you can find our community 
+          <p>On our application you can find our Tandem community 
          and filter users by your city and language choices.</p>
           </div>
          
          <div>
-         <p>The application gives you the opportunity to contact each other 
-        to chat on the application or to meet in real life</p>
+         <p>The application gives you the opportunity to contact your future Tandem,  
+        chat on the application or meet in real life</p>
         
          </div>
 
-        <p>The application gives you the opportunity to contact each other 
-        to chat on the application or to meet in real life</p>
 
         <div>
           <p>This project was realized by Vero and Oriane in 1 week during the Ironhack bootcamp </p>
